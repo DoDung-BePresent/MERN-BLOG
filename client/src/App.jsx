e63f50215e2162c9866f4968bb0a1cce907f6dd4
DoDung <doquangdung1782004@gmail.com>
2024-05-03 18:17:09 +0700

    Cau hinh Navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+
+// components
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
